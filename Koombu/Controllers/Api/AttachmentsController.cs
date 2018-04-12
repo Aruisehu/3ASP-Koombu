07ed2960b02f467f0dd5e8cafe1e4d6e405308d4
Yohann Jolain <yohann.jolain@supinfo.com>
2018-04-12 10:33:33 +0200

    Added web services

---
+++ Koombu/Controllers/Api/AttachmentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Koombu.Data;
+using Koombu.Models;
+
+namespace Koombu.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Attachments")]
+    public class AttachmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttachmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Attachments
+        [HttpGet]
+        public IEnumerable<Attachment> GetAttachments()
+        {
+            return _context.Attachments;
+        }
+
+        // GET: api/Attachments/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAttachment([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var attachment = await _context.Attachments.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (attachment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(attachment);
+        }
+
+        // PUT: api/Attachments/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAttachment([FromRoute] string id, [FromBody] Attachment attachment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != attachment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(attachment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttachmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Attachments
+        [HttpPost]
+        public async Task<IActionResult> PostAttachment([FromBody] Attachment attachment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Attachments.Add(attachment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAttachment", new { id = attachment.Id }, attachment);
+        }
+
+        // DELETE: api/Attachments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttachment([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var attachment = await _context.Attachments.SingleOrDefaultAsync(m => m.Id == id);
+            if (attachment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Attachments.Remove(attachment);
+            await _context.SaveChangesAsync();
+
+            return Ok(attachment);
+        }
+
+        private bool AttachmentExists(string id)
+        {
+            return _context.Attachments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Koombu/Controllers/Api/UsersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Koombu.Data;
+using Koombu.Models;
+
+namespace Koombu.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<ApplicationUser> GetApplicationUsers()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationUser([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(applicationUser);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationUser([FromRoute] string id, [FromBody] ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != applicationUser.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(applicationUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationUser([FromBody] ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ApplicationUsers.Add(applicationUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApplicationUser", new { id = applicationUser.Id }, applicationUser);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationUser([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.ApplicationUsers.Remove(applicationUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(applicationUser);
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
+    }
+}
